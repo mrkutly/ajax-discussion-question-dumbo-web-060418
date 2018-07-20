306f24f4d1c52c2fa4a7fe9eb66fee4d38fb2913
mark sauer-utley <mark.utley.713@gmail.com>
2018-07-20 09:33:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
-
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
+  const fullname = document.getElementById("fullname");
+  const picture = document.querySelector('#profile_picture');
+  const email = document.querySelector('#email');
+  const street = document.querySelector('#street')
+  const state = document.querySelector('#state')
+  const city = document.querySelector('#city')
+  const postcode = document.querySelector('#postcode')
+  const phone = document.querySelector('#phone')
+  const cell = document.querySelector('#cell')
+  const birthDay = document.querySelector('#date_of_birth')
+  const newButton = document.querySelector('#new-button')
+
+
+
+  newButton.addEventListener('click', function() {
+   fetch('https://randomuser.me/api/')
+    .then( res => res.json() )
+    .then(function(json){
+      console.log(json)
+      let person = json['results'][0]
+      let name = person.name
+
+      fullname.innerText = `${name.title} ${name.first} ${name.last}`
+      picture.setAttribute('src', person['picture']['large'])
+      email.innerText = person.email
+      street.innerText = person['location']['street']
+      state.innerText = person['location']['state']
+      city.innerText = person['location']['city']
+      postcode.innerText = person['location']['postcode']
+      cell.innerText = person['cell']
+      phone.innerText = person['phone']
+      birthDay.innerText = person['dob']['date']
+    })
+  })
+
 });
+// location
